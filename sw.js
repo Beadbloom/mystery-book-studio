a71a4ce53cd930172d78e5071c17e86f1ca10488
Beadbloom <admin@beadbloom.co.uk>
2026-09-11 22:45:50 +0100

    Bump app cache for cover and live preview update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='mystery-book-studio-v12';
+const C='mystery-book-studio-v13';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./app.js','./manifest.webmanifest'])))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request))));
