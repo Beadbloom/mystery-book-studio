d3ba4669d3f1daf57bd9da5a4d0f8f548b13c666
Beadbloom <admin@beadbloom.co.uk>
2026-09-11 16:14:35 +0100

    Bump cache for crisp artwork preservation update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='mystery-book-studio-v4';
+const C='mystery-book-studio-v5';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./app.js','./linefix.js','./savefix.js','./manifest.webmanifest'])))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
