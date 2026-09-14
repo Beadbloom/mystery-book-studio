69f58826436566d61c38bc8ef5546857bca66c98
Beadbloom <admin@beadbloom.co.uk>
2026-09-14 21:59:22 +0100

    Refresh cache for v16 exact swatch workflow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='mystery-book-studio-v14';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./app.js','./manifest.webmanifest'])))});
+const C='mystery-book-studio-v16';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./app-v16.js','./manifest.webmanifest'])))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request))));
