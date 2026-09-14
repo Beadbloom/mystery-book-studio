9a31e88a111180caccc60b83430c609e16ac0c51
Beadbloom <admin@beadbloom.co.uk>
2026-09-14 22:20:44 +0100

    Refresh cache for v18 gallery and spread pagination

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='mystery-book-studio-v17';
+const C='mystery-book-studio-v18';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./app-v17.js','./manifest.webmanifest'])))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(C).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request))));
